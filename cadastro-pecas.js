0f13bdc554c016588f2a23c059e3720112a1e151
Renan Pereira <renan.rpo@hotmail.com>
2021-10-22 20:45:54 -0300

    software de cadastro de peças

---
+++ cadastro-pecas.js
@@ -0,0 +1,27 @@
+console.log ("Bem vindo ao software de cadastro de peças");
+
+let peso = 120;  // peso da peça
+
+if (peso > 100) {
+    console.log("Peso dentro dos parâmetros, prosseguir com cadastro")
+} else {
+    console.log("Cadastro não pode ser efetuado, a peça não tem o peso minimo exigido")
+}
+
+let listaPecas = ["Arruela","Parafuso","Porca","Rolimã","Broca","Sextavado","Bloco","Mola","bsp08"];
+
+let numeroPecas = listaPecas.length;
+
+if (numeroPecas <10) {
+    console.log("Espaço disponivel, prossiga com o cadastro")
+} else {
+    console.log("Sem espaço disponvivel na lista, capacidade máximajá foi atingida")
+}
+
+let nomePeca = listaPecas [1]  // peça a ser selecionada dentro das opções da linha 11
+
+if (nomePeca.length >3) {
+    console.log("Nome válido, cadastro efetuado")
+} else {
+    console.log("Nome inválido, o nome deve ter mais que 3 caracteres")
+}
